16d0f951bc08953d2133e9c2240b74e3e0d98c6e
MrZhang365 <MrZhang365@outlook.com>
2024-02-11 15:15:50 +0800

    添加友谊星空

---
+++ public/the-sky-of-friendship/365.js
@@ -0,0 +1,57 @@
+async function getFriends() {
+    return (await get('/api/home')).author.friends
+}
+
+function getPaper() {
+    // 获取到画布（纸）
+    return document.getElementById('sky')
+}
+
+function whichFriend(x, y, friendsXY) {
+    for (let friend in friendsXY) {
+        const friendX = friendsXY[0]
+        const friendY = friendsXY[1]
+
+        if (x >= friendX && x <= friendX + 5 && y >= friendY && y <= friendY + 5) return friend
+    }
+    return null
+}
+
+async function init() {
+    const paper = getPaper()
+    const ctx = paper.getContext('2d')
+    const friends = await getFriends()
+    const friendsXY = {}
+    for (let key of Object.keys(friends).keys) {
+        friendsXY[key] = []
+    }
+
+    paper.width = window.innerWidth
+    paper.height = window.innerHeight
+
+    ctx.height = paper.height
+    ctx.width = paper.width
+
+    ctx.fillStyle = '#000000'
+    ctx.fillRect(0, 0, paper.width, paper.height)
+
+    ctx.fillStyle = '#FFFFFF'
+    for (let i = 0; i < Object.keys(friends).length; i++) {
+        const x = Math.random() * ctx.width
+        const y = Math.random() * ctx.height
+        ctx.fillRect(x, y, 5, 5)
+
+        friendsXY[friends[Object.keys(friends)[i]]] = [x, y]
+    }
+
+    paper.onclick = e => {
+        const result = whichFriend(e.clientX, e.clientY, friendsXY)
+        if (result === null) return e.preventDefault()
+        const a = document.createElement('a')
+        a.href = friends[result]
+        a.target = '_blank'
+        a.click()
+    }
+}
+
+init()
