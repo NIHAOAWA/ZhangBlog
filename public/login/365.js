2cc4fb9742a4a20286cb3afdb515fbe702d97814
NIHAOAWA <142315379+NIHAOAWA@users.noreply.github.com>
2024-05-17 19:01:58 +0800

    Update 365.js

--- public/login/365.js
+++ public/login/365.js
@@ -2,4 +2,4 @@ function $ (id) {
     return document.getElementById(id)
 }
 
-$('login').onclick = () => location.href = '/api/auth/login'
+$('login').onclick = () => location.href = 'https://github.com/login/oauth/authorize?client_id=Ov23lijf9jEV6uZZS4Mf'
